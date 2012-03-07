090c699417bbd7541006dd27249dbfc6e40eee55
Brad Kimmel <intrepidca@gmail.com>
2012-03-06 23:10:04 -0500

    Added comments.

--- sortable/src/ca/eandb/sortable/ProductReader.java
+++ sortable/src/ca/eandb/sortable/ProductReader.java
@@ -6,11 +6,20 @@ package ca.eandb.sortable;
 import java.io.Reader;
 
 /**
+ * Represents an object capable of producing <code>Product</code>s from a
+ * provided input source.
  * @author Brad Kimmel
- *
  */
 public interface ProductReader {
 	
+	/**
+	 * Reads the <code>Product</code>s from the provided source and enumerates
+	 * them to the provided <code>ProductVisitor</code>.
+	 * @param in The <code>Reader</code> to read the <code>Product</code>s from.
+	 * @param visitor The <code>ProductVisitor</code> to use to enumerate the
+	 * 		<code>Product</code>s.
+	 * @throws Exception If an error occurs while reading.
+	 */
 	void read(Reader in, ProductVisitor visitor) throws Exception;
 
 }
