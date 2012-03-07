39c868a4e1426e1ef2269b5b386f8594a00820db
Brad Kimmel <intrepidca@gmail.com>
2012-03-06 23:46:30 -0500

    Added javadoc comments.

--- sortable/src/ca/eandb/sortable/ProductVisitor.java
+++ sortable/src/ca/eandb/sortable/ProductVisitor.java
@@ -4,10 +4,15 @@
 package ca.eandb.sortable;
 
 /**
+ * A visitor object for iterating through a collection of <code>Product</code>s.
  * @author Brad Kimmel
  */
 public interface ProductVisitor {
 
+	/**
+	 * Visites a <code>Product</code>.
+	 * @param product The <code>Product</code> to visit.
+	 */
 	void visit(Product product);
 	
 }
